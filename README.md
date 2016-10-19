1d31496357376d2a39b0c0e0a40bf0f911bcfc71
Farhan Wazir <farhan.wazir@gmail.com>
2016-10-19 07:36:33 +0500

    Field Ready to publish

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Laravel Map Form Field
+
--- src/FarhanWazir/LaravelMapFormField/MapField.php
+++ src/FarhanWazir/LaravelMapFormField/MapField.php
@@ -10,51 +10,92 @@
 Form::macro('map', function($name, $value = null, $settings = false, $attributes = false){
     //Default settings
     $default_settings = [
-        'search' => true,
-        'latLng' => true,
-        'search-align' => 'top_center',
-        'latLng-align' => 'bottom_center'
+        'search' => false,
+        'latlng' => false,
+        /*'search-align' => 'top_center',
+        'latLng-align' => 'bottom_center'*/
     ];
 
     //TODO: Merge user settings with default
-    $settings = $default_settings;
+    $settings = is_array($settings) ? array_merge($default_settings, $settings) : $default_settings;
 
-    //search field
-    Form::text('fw_map_form_field_place_search', null, ['placeholder' => 'Search place', 'id' => 'fw-map-form-field-place-search']);
-    Form::text('fw_map_form_field_lat', null, ['placeholder' => 'Latitude', 'id' => 'fw-map-form-field-lat']);
-    Form::text('fw_map_form_field_lng', null, ['placeholder' => 'Longitude', 'id' => 'fw-map-form-field-lng']);
+    $field_name_search = 'fw_map_form_field_place_search';
+    $field_name_lat = 'fw_map_form_field_lat';
+    $field_name_lng = 'fw_map_form_field_lng';
+
+    $field_class_search = (is_array($attributes) && array_key_exists('search', $attributes) && array_key_exists('class', $attributes['search']))?
+        $attributes['search']['class'] : '';
+    $field_class_lat = (is_array($attributes) && array_key_exists('latitude', $attributes) && array_key_exists('class', $attributes['latitude']))?
+        $attributes['latitude']['class'] : '';
+    $field_class_lng = (is_array($attributes) && array_key_exists('longitude', $attributes) && array_key_exists('class', $attributes['longitude']))?
+        $attributes['longitude']['class'] : '';
+
+    if(is_array($attributes)){
+        if(array_key_exists('latitude', $attributes) && array_key_exists('name', $attributes['latitude']))
+            $field_name_lat = $attributes['latitude']['name'];
+        if(array_key_exists('longitude', $attributes) && array_key_exists('name', $attributes['longitude']))
+            $field_name_lng = $attributes['longitude']['name'];
+    }
+
+    $fields = '';
+    if($settings['search'])
+        $fields .= Form::text($field_name_search, null, ['placeholder' => 'Search place', 'id' => 'fw-map-form-field-place-search', 'class' => $field_class_search]);
+    if($settings['latlng']){
+        $fields .= Form::text($field_name_lat, null, ['placeholder' => 'Latitude', 'id' => 'fw-map-form-field-lat', 'class' => $field_class_lat]);
+        $fields .= Form::text($field_name_lng, null, ['placeholder' => 'Longitude', 'id' => 'fw-map-form-field-lng', 'class' => $field_class_lng]);
+    }else{
+        $fields .= '<input type="hidden" id="fw-map-form-field-lat"><input type="hidden" id="fw-map-form-field-lng">';
+    }
 
     //Map facade, see detail at https://github.com/farhanwazir/laravelgooglemaps
     $GMaps = app()->make('GMaps');
     $GMaps->injectControlsInTopCenter = ['document.getElementById("fw-map-form-field-place-search")'];
     $GMaps->injectControlsInBottomCenter = ['document.getElementById("fw-map-form-field-lat")', 'document.getElementById("fw-map-form-field-lng")'];
 
+    $marker = array();
+    $marker['draggable'] = true;
+    $marker['ondragend'] = '
+        /*iw_'. $GMaps->getMapName() .'.close();*/
+        iw_'. $GMaps->getMapName() .'.setContent("Loading... Please wair or if you seeing this since long then move marker or map.");
+        reverseGeocode(event.latLng, function(status, result, mark){
+            if(status == 200){
+                iw_'. $GMaps->getMapName() .'.setContent(result);
+                iw_'. $GMaps->getMapName() .'.open('. $GMaps->getMapName() .', marker_0);
+            }else{
+                iw_'. $GMaps->getMapName() .'.close();
+            }
+        }, this);
+        '. $GMaps->injectControlsInBottomCenter[0] .'.value = event.latLng.lat();
+        '. $GMaps->injectControlsInBottomCenter[1] .'.value = event.latLng.lng();
+        '. $GMaps->getMapName() .'.setCenter(event.latLng);
+        ';
+
     $config = array();
     $config['map_height'] = "100%";
-    $config['center'] = 'Clifton, Karachi';
-    $config['onboundschanged'] = 'if (!centreGot) {
-            var mapCentre = map.getCenter();
+    $config['center'] = 'auto';
+    $config['onboundschanged'] = 'var centreGot = false; if (!centreGot) {
+            var mapCentre = '. $GMaps->getMapName() .'.getCenter();
             marker_0.setOptions({
                 position: new google.maps.LatLng(mapCentre.lat(), mapCentre.lng())
             });
         }
-        centreGot = true;';
+        centreGot = true;
+        event = {latLng: mapCentre};
+        '.$marker['ondragend'];
+    $config['places'] = $settings['search'];
+    $config['placesAutocompleteInputID'] = 'fw-map-form-field-place-search';
+    $config['placesAutocompleteOnChange'] = '
+    marker_0.setPosition(place.geometry.location);
+     '.$marker['ondragend'].' ';
+    $config['palcesAutoCompleteOnChangeFailed'] = '
+        alert("No place found.");
+    ';
+
     $GMaps->initialize($config);
 
-    $marker = array();
-    $marker['draggable'] = true;
-    $marker['ondragend'] = '
-        iw_'. GMaps::map_name .'.close();
-        reverseGeocode(event.latLng, function(status, result, mark){
-            if(status == 200){
-                iw_'. GMaps::map_name .'.setContent(result);
-                iw_'. GMaps::map_name .'.open('. GMaps::map_name .', mark);
-            }
-        }, this);
-        ';
     $GMaps->add_marker($marker);
 
     $map = $GMaps->create_map();
-    return $map['js'] . $map['html'];
+    return $fields . $map['js'] . $map['html'];
 });
 
